1c7e59a23c652d614315dd7e7c4e5e0cd4ab6f42
Meri  Sumic <merisumic@gmail.com>
2025-09-18 12:17:00 +0200

    Dovršena dva zadatka

---
+++ DoPred05/GodinaIMjesec/Program.cs
@@ -0,0 +1,40 @@
+﻿/* 4.Godina i mjesec
+
+Unesi godinu i mjesec.
+
+Ako je godina prijestupna i mjesec je veljača → “Veljača ima 29 dana.”
+
+Ako godina nije prijestupna i mjesec je veljača → “Veljača ima 28 dana.”
+
+Ako je mjesec 4, 6, 9 ili 11 → “Mjesec ima 30 dana.”
+
+Inače → “Mjesec ima 31 dan.”
+
+(ovdje moraš kombinirati uvjete i dobro paziti na “osim ako” kod veljače) */
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Unesi godinu: ");
+int godina = int.Parse(Console.ReadLine());
+
+Console.Write("Unesi naziv mjeseca: ");
+string mjesec = Console.ReadLine();
+
+if ((godina % 400 == 0 || (godina % 4 == 0 && godina % 100 != 0)) && mjesec == "veljača")
+{
+    Console.WriteLine("Veljača ima 29 dana");
+}
+else if ((godina % 400 != 0 || (godina % 4 != 0 && godina % 100 == 0)) && mjesec == "veljača")
+{
+    Console.WriteLine("Veljača ima 28 dana");
+}
+else if (mjesec == "travanj" || mjesec == "lipanj" || mjesec == "rujan" || mjesec == "studeni")
+{
+    Console.WriteLine("Mjesec ima 30 dana");
+}
+else
+{
+    Console.WriteLine("Mjesec ima 31 dan");
+}
+
--- DoPred05/SpecijalanBroj/Program.cs
+++ DoPred05/SpecijalanBroj/Program.cs
@@ -1,7 +1,6 @@
 ﻿/* Unesi cijeli broj.
 Broj je specijalan ako je:
-- pozitivan,
-- djeljiv s 3,
+- pozitivan i djeljiv s 3,
 - ali nije djeljiv s 9,
 - osim ako je također paran → onda opet je specijalan.
 
@@ -9,6 +8,19 @@ Inače → “Broj nije specijalan.”
 */
 
 
+Console.Write("Unesi cijeli broj: ");
+int broj = int.Parse(Console.ReadLine());
 
-
+if (broj % 2 == 0) // iznimka: svaki paran broj je specijalan
+{
+    Console.WriteLine("Broj je specijalan");
+}
+else if (broj > 0 && broj % 3 == 0 && broj % 9 != 0) // osnovno pravilo
+{
+    Console.WriteLine("Broj je specijalan");
+}
+else
+{
+    Console.WriteLine("Broj nije specijalan");
+}
 
