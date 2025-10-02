584be38db840c98f2c59648007c02e5adec48b3f
Meri  Sumic <merisumic@gmail.com>
2025-10-02 16:33:02 +0200

    Vjezba

---
+++ Predavanje09/Zadatak1_vjezba/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Zadatak 1: Napravi program koji traži unos rečenice od korisnika, a zatim ispisuje
+koliko samoglasnika i koliko suglasnika rečenica sadrži. Program treba biti otporan
+na greške pri unosu. */
+
+List<char> samoglasnici = new List<char>();
+List<char> sumoglasnici = new List<char>();
+
+while (true)
+{
+	try
+	{
+
+	}
+	catch (Exception)
+	{
+
+		throw;
+	}
+}
