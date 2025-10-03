fc555b6792a0bb73d35687a7f40531cd522725d8
Meri  Sumic <merisumic@gmail.com>
2025-10-03 15:03:11 +0200

    vjezbanje

---
+++ Predavanje09/Zadatak4/Program.cs
@@ -0,0 +1,31 @@
+﻿//Zadatak 3: Napravite program koji traži unos riječi od korisnika. Program treba
+//nastaviti tražiti unos dok korisnik ne unese riječ koja je već prethodno unesena.
+//Nakon toga, program treba ispisati riječ koja se ponovila i završiti izvršavanje
+
+using System.Globalization;
+
+List<string> uneseneRijeci = new List<string>();
+
+while (true)
+{
+	try
+	{
+        Console.Write("Unesi riječ: ");
+        string rijec = Console.ReadLine();
+
+        if (uneseneRijeci.Contains(rijec))
+		{
+			Console.WriteLine("Ponovljena rijec je: ");
+			break;
+		}
+		else
+		{
+			uneseneRijeci.Add(rijec);
+		}
+	}
+	catch (Exception)
+	{
+
+		throw;
+	}
+}
--- Predavanje09/Zadatak_Hare2/Program.cs
+++ Predavanje09/Zadatak_Hare2/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿//Zadatak 2:Napravi program koji traži unos brojeva od korisnika dok zbroj svih
+//unesenih brojeva ne premaši 100. Kada se to dogodi, program treba ispisati konačni
+//zbroj. 
+
+
+using System.Linq.Expressions;
+
+int zbroj = 0;
+
+while (zbroj < 100)
+{
+    try
+    {
+        Console.Write("Unesi broj: ");
+        int broj = int.Parse(Console.ReadLine());
+        zbroj += broj;
+    }
+    catch (Exception ex)
+    {
+        Console.Write("Ups...");
+    }
+}
+
+
+Console.WriteLine("zbroj je: " + zbroj);
